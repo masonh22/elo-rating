4df5c9d78d13fb6902e2db9b82b402863ba0524a
Mason Hall <mhall22990@gmail.com>
2020-08-27 15:01:55 -0400

    Updates

---
+++ precache-manifest.5e10fb2006f96b5876bec63c67b87d58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681856749bac3ca689359f35a228c47f",
+    "url": "/elo-rating/index.html"
+  },
+  {
+    "revision": "a455d78ea7be82746c04",
+    "url": "/elo-rating/static/css/main.c421e6b4.chunk.css"
+  },
+  {
+    "revision": "3771dc2882630a4d28ed",
+    "url": "/elo-rating/static/js/2.2c7dc985.chunk.js"
+  },
+  {
+    "revision": "10eb1ae5fdc43f81bbbfc6e2d1586269",
+    "url": "/elo-rating/static/js/2.2c7dc985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a455d78ea7be82746c04",
+    "url": "/elo-rating/static/js/main.6315b50c.chunk.js"
+  },
+  {
+    "revision": "ad271b59d9238ac2006b",
+    "url": "/elo-rating/static/js/runtime-main.0652fda2.js"
+  }
+]);
